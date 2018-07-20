e5fe7bfd2ec808ac1497e2a31a8380e97c83983b
Rui Gonçalves <ruippeixotog@gmail.com>
2018-07-20 18:25:08 +0100

    Add remaining State classes

--- build.sbt
+++ build.sbt
@@ -1,3 +1,7 @@
 name := "icfpc2018"
 
 scalaVersion := "2.12.6"
+
+libraryDependencies ++= Seq(
+  "org.specs2" %% "specs2-core" % "4.2.0"
+)
--- src/main/scala/icfpc2018/State.scala
+++ src/main/scala/icfpc2018/State.scala
@@ -37,23 +37,61 @@ sealed trait Command {
   def encoded: Vector[Byte]
 }
 
-case object Halt {
-  val encoded = Vector(0xFF)
+case object Halt extends Command {
+  val encoded = Vector(11111111.b)
 }
 
-case object Wait {
-  val encoded = Vector(0xFE)
+case object Wait extends Command {
+  val encoded = Vector(11111110.b)
 }
 
-case object Flip {
-  val encoded = Vector(0xFD)
+case object Flip extends Command {
+  val encoded = Vector(11111101.b)
 }
 
-case class SMove(lld: LLD) {
-  val encoded = Vector()
+case class SMove(lld: LLD) extends Command {
+  val encoded = Vector((100 + lld.a.encoded << 4).toByte, (lld.i + 15).toByte)
 }
 
-case class LLD(a: Dir, dist: Int)
+case class LMove(sld1: SLD, sld2: SLD) extends Command {
+  val encoded = Vector((1100 + sld1.a.encoded << 4 + sld2.a.encoded << 6).toByte, (sld1.i + sld2.i << 4).toByte)
+}
+
+case class FusionP(nd: NCD) extends Command {
+  val encoded = Vector((111 + nd.encoded << 3).toByte)
+}
+
+case class FusionS(nd: NCD) extends Command {
+  val encoded = Vector((110 + nd.encoded << 3).toByte)
+}
+
+case class Fission(nd: NCD, m: Int) extends Command {
+  val encoded = Vector((101 + nd.encoded << 3).toByte, m.toByte)
+}
+
+case class Fill(nd: NCD) extends Command {
+  val encoded = Vector((11 + nd.encoded << 3).toByte)
+}
+
+case class LLD(a: Dir, i: Int) {
+  require(i >= -15 && i <= 15)
+}
+
+case class SLD(a: Dir, i: Int) {
+  require(i >= -5 && i <= 5)
+}
+
+case class NCD(dx: Int, dy: Int, dz: Int) {
+  def encoded: Int = (dx + 1) * 9 + (dy + 1) * 3 + (dz + 1)
+
+  require(math.abs(dx) <= 1)
+  require(math.abs(dy) <= 1)
+  require(math.abs(dz) <= 1)
+  require {
+    val mlen = math.abs(dx) + math.abs(dy) + math.abs(dz)
+    mlen >= 1 && mlen <= 2
+  }
+}
 
 sealed trait Dir {
   def encoded: Byte
@@ -64,9 +102,9 @@ case object X extends Dir {
 }
 
 case object Y extends Dir {
-  val encoded = 0
+  val encoded = 10.b
 }
 
 case object Z extends Dir {
-  val encoded = 0
+  val encoded = 11.b
 }
