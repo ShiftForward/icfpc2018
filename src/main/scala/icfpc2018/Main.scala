3d96b0e7ae8314d874a9e718fa32db0109a423df
João Costa <jdpc557@gmail.com>
2018-07-22 19:10:58 +0100

    Simplify Main

--- src/main/scala/icfpc2018/Main.scala
+++ src/main/scala/icfpc2018/Main.scala
@@ -8,14 +8,8 @@ import scala.collection.JavaConverters._
 import icfpc2018.solver._
 import scala.util.{ Failure, Success, Try }
 
-object LightningMain extends App {
-
-  val models = Files.list(Paths.get("models", "lightning")).iterator().asScala.toList
-    .filter(_.toString.endsWith(".mdl"))
-    .sortBy(_.toString)
-  val solver: Solver = GreedySolver
+trait Main extends App {
   def validate(model: Matrix, solution: List[Command]) = Try(Simulator.runAndValidate(model, solution))
-  //def validate(model: Matrix, solution: List[Command]) = true
 
   def time[T](f: => T): (T, Long) = {
     val startTime = System.currentTimeMillis()
@@ -32,6 +26,14 @@ object LightningMain extends App {
     bos.close()
     outputFilename
   }
+}
+
+object LightningMain extends Main {
+
+  val models = Files.list(Paths.get("models", "lightning")).iterator().asScala.toList
+    .filter(_.toString.endsWith(".mdl"))
+    .sortBy(_.toString)
+  val solver: Solver = GreedySolver
 
   models.foreach { modelPath =>
     println("Parsing model " + modelPath.toString)
@@ -52,7 +54,7 @@ object LightningMain extends App {
   }
 }
 
-object FullMain extends App {
+object FullMain extends Main {
 
   val models = Files.list(Paths.get("models", "full")).iterator().asScala.toList
     .filter(_.toString.endsWith(".mdl"))
@@ -68,25 +70,8 @@ object FullMain extends App {
   val assemblySolver: Solver = GreedySolver
   val disassemblySolver: RebuilderSolver = TracerSolver
   val reassemblySolver: RebuilderSolver = TracerSolver
-  def validate(model: Matrix, solution: List[Command]) = Try(Simulator.runAndValidate(model, solution))
-  //def validate(model: Matrix, solution: List[Command]) = true
 
-  def time[T](f: => T): (T, Long) = {
-    val startTime = System.currentTimeMillis()
-    val res = f
-    val endTime = System.currentTimeMillis()
-    (res, endTime - startTime)
-  }
-
-  def export(commands: List[Command], filename: String): String = {
-    val output = commands.flatMap(_.encoded).toArray
-    val outputFilename = "output/" + filename.take(5) + ".nbt"
-    val bos = new BufferedOutputStream(new FileOutputStream(outputFilename))
-    bos.write(output)
-    bos.close()
-    outputFilename
-  }
-
+  println("Running Assembly Problems...")
   assemblyModels.foreach { modelPath =>
     println("Parsing model " + modelPath.toString)
     val (model, parseTime) = time(Matrix.fromMdl(modelPath.toFile))
@@ -97,25 +82,28 @@ object FullMain extends App {
     validModel match {
       case Success(st) =>
         println(s"Validated solution in ${validationTime}ms. Energy was ${st.energy}")
-        val (outputFilename, exportedTime) = time(export(solution, modelPath.getFileName.toString))
-        println(s"Exported to $outputFilename in ${exportedTime}ms")
       case Failure(ex) =>
         println(s"Failed validation in ${validationTime}ms with ${ex.getClass.getName}")
     }
+    val (outputFilename, exportedTime) = time(export(solution, modelPath.getFileName.toString))
+    println(s"Exported to $outputFilename in ${exportedTime}ms")
     println("-----------------------------------------------------------")
   }
 
+  println("Running Disassembly Problems...")
   disassemblyModels.foreach { modelPath =>
     println("Parsing model " + modelPath.toString)
     val (model, parseTime) = time(Matrix.fromMdl(modelPath.toFile))
     println(s"Parsed ${model.voxels.size} in ${parseTime}ms")
     val (solution, solvedTime) = time(disassemblySolver.solve(model, Matrix(model.dimension)))
     println(s"Solved with ${solution.size} commands in ${solvedTime}ms")
+    // TODO validate this
     val (outputFilename, exportedTime) = time(export(solution, modelPath.getFileName.toString))
     println(s"Exported to $outputFilename in ${exportedTime}ms")
     println("-----------------------------------------------------------")
   }
 
+  println("Running Reassembly Problems...")
   reassemblyModels.foreach {
     case (srcModelPath, dstModelPath) =>
       println("Parsing model " + srcModelPath.toString)
@@ -125,6 +113,7 @@ object FullMain extends App {
       val (dstModel, dstParseTime) = time(Matrix.fromMdl(dstModelPath.toFile))
       println(s"Parsed ${dstModel.voxels.size} in ${dstParseTime}ms")
       val (solution, solvedTime) = time(reassemblySolver.solve(srcModel, dstModel))
+      // TODO validate this
       println(s"Solved with ${solution.size} commands in ${solvedTime}ms")
       val (outputFilename, exportedTime) = time(export(solution, srcModelPath.getFileName.toString))
       println(s"Exported to $outputFilename in ${exportedTime}ms")
