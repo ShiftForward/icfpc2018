5cff7f3ca5ba055a5e705f4e8b0461efa350a2bd
João Costa <jdpc557@gmail.com>
2018-07-22 19:24:05 +0100

    Make Tracer use a minimal bounding box

--- src/main/scala/icfpc2018/solver/TracerSolver.scala
+++ src/main/scala/icfpc2018/solver/TracerSolver.scala
@@ -3,21 +3,31 @@ package icfpc2018.solver
 import scala.collection.mutable
 
 import icfpc2018._
-import icfpc2018.solver.SolverDSL.{ ReleaseHarmonics, RequireHarmonics, SolverCommand }
+import icfpc2018.solver.SolverDSL.{ RawCommand, ReleaseHarmonics, RequireHarmonics, SolverCommand }
+import icfpc2018.solver.pathing.AStarPathFinder
 
 object TracerSolver extends PartialSolver {
   def partialSolve(srcModel: Matrix, dstModel: Matrix, from: Coord): (List[SolverCommand], Matrix, Coord) = {
     var dx = 1
     var dz = 1
-    var x = from.x
-    var y = from.y
-    var z = from.z
     val commands = mutable.ListBuffer[SolverCommand]()
+
+    val minX = (srcModel.voxels ++ dstModel.voxels).minBy(_.x).x
+    val minZ = (srcModel.voxels ++ dstModel.voxels).minBy(_.z).z
+
     val maxX = (srcModel.voxels ++ dstModel.voxels).maxBy(_.x).x
     val maxY = (srcModel.voxels ++ dstModel.voxels).maxBy(_.y).y
     val maxZ = (srcModel.voxels ++ dstModel.voxels).maxBy(_.z).z
-    val dimension = math.max(maxX, maxZ) + 1
-    val len = (maxX + 1) * (maxY + 1) * (maxZ + 1)
+
+    val len = (maxX + 1 - (minX - 1)) * (maxY + 1) * (maxZ + 1 - (minZ - 1))
+
+    commands ++=
+      new AStarPathFinder(srcModel).findPath(from, Coord(minX - 1, 0, minZ - 1)).map(RawCommand)
+
+    var x = minX - 1
+    var y = 0
+    var z = minZ - 1
+
     var currModel = srcModel
     var highHarmonics = false
 
@@ -27,14 +37,14 @@ object TracerSolver extends PartialSolver {
       var ny = y
       var nx = x
       nz += dz
-      if (nz >= (maxZ + 1) || nz < 0) {
-        nz = if (nz == (maxZ + 1)) maxZ else 0
+      if (nz >= (maxZ + 1) || nz < (minZ - 1)) {
+        nz = if (nz == (maxZ + 1)) maxZ else (minZ - 1)
         nx += dx
         dz = if (dz == 1) -1 else 1
       }
 
-      if (nx >= (maxX + 1) || nx < 0) {
-        nx = if (nx == (maxX + 1)) maxX else 0
+      if (nx >= (maxX + 1) || nx < (minX - 1)) {
+        nx = if (nx == (maxX + 1)) maxX else (minX - 1)
         ny += 1
         dx = if (dx == 1) -1 else 1
       }
