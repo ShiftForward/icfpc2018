0892c755704c3aab1c2f1ee31ef31e057758531b
Beatriz Magalhães <mbeatrizmagalhaes@gmail.com>
2018-07-22 16:43:58 +0100

    Add method to matrix to check if volume is totally full or void

--- src/main/scala/icfpc2018/Matrix.scala
+++ src/main/scala/icfpc2018/Matrix.scala
@@ -4,7 +4,7 @@ import java.io.File
 import java.nio.file.Files
 
 import scala.annotation.tailrec
-import scala.collection.BitSet
+import scala.collection.{ BitSet, mutable }
 
 import icfpc2018.Matrix.CoordSet
 
@@ -21,6 +21,74 @@ case class Matrix(
   lazy val centerOfMass: Coord = {
     val (x, y, z) = voxels.foldLeft((0, 0, 0)) { case ((sumX, sumY, sumZ), c) => (sumX + c.x, sumY + c.y, sumZ + c.z) }
     Coord(x / voxels.size, y / voxels.size, z / voxels.size)
+  }
+
+  lazy val sumOfFilled: Map[Coord, Int] = {
+    val map = mutable.Map.empty[Coord, Int]
+
+    def value(x: Int, y: Int, z: Int): Int = {
+      val c = Coord(x, y, z)
+      if (!validateCoord(c)) 0
+      else map.get(c).get
+    }
+
+    (0 until dimension).map { x =>
+      (0 until dimension).map { y =>
+        (0 until dimension).map { z =>
+          val v = if (voxels.contains(Coord(x, y, z))) 1 else 0
+          val vx = value(x - 1, y, z)
+          val vy = value(x, y - 1, z)
+          val vz = value(x, y, z - 1)
+          val vxyz = value(x - 1, y - 1, z - 1)
+          val vxy = value(x - 1, y - 1, z)
+          val vxz = value(x - 1, y, z - 1)
+          val vyz = value(x, y - 1, z - 1)
+
+          val sum = v + vx + vy + vz + vxyz
+          val minus = vxy + vxz + vyz
+
+          map.put(Coord(x, y, z), (v + vx + vy + vz + vxyz - vxy - vxz - vyz))
+        }
+      }
+    }
+    map.toMap
+  }
+
+  /**
+   *
+   * @param c1 one corner of the "cube"
+   * @param c2 the diagonal corner of the "cube"
+   * @return Option[Voxel] -> Some if full volume has same value
+   */
+  def isVoidOrFull(c1: Coord, c2: Coord): Option[Voxel] = {
+    require(validateCoord(c1) && validateCoord(c2), "c1 and c2 must be valid")
+    def value(x: Int, y: Int, z: Int): Int = {
+      val c = Coord(x, y, z)
+      if (!validateCoord(c)) 0
+      else sumOfFilled.get(c).get
+    }
+
+    val xMax = math.max(c1.x, c2.x)
+    val xMin = math.min(c1.x, c2.x) - 1
+    val yMax = math.max(c1.y, c2.y)
+    val yMin = math.min(c1.y, c2.y) - 1
+    val zMax = math.max(c1.z, c2.z)
+    val zMin = math.min(c1.z, c2.z) - 1
+
+    val v1 = value(xMin, yMax, zMax)
+    val v2 = value(xMax, yMax, zMin)
+    val v3 = value(xMax, yMin, zMax)
+    val v4 = value(xMax, yMin, zMin)
+    val v5 = value(xMin, yMin, zMax)
+    val vmin = value(xMin, yMin, zMin)
+    val vmax = value(xMax, yMax, zMax)
+
+    val sum = vmax - v1 - v2 - v3 + v4 + v5 + vmin
+    val volume = (xMax - xMin) * (yMax - yMin) * (zMax - zMin)
+
+    if (sum == 0) Some(Void)
+    else if (sum == volume) Some(Full)
+    else None
   }
 
   def validateCoord(coord: Coord): Boolean =
